c9837683cc30a99a7ae1a94aafb9524d13c3083c
seliana <cany96@bk.ru>
2021-07-03 16:48:33 +0300

    feat: hide subscribe from on scroll

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+
+let lastScrollTop = window.pageYOffset
+
+/**
+ * Submit subscribe form handler
+ */
+function submitForm() {
+  alert('Your are subscribed to news letters!')
+}
+
+/**
+ * Check if subscribe form should be visible on mobile screen
+ */
+function checkSubscribeVisibility() {
+  const currentScroll = window.pageYOffset
+  const subscribeForm = document.getElementById('subscribe-form')
+
+  if (currentScroll > lastScrollTop) {
+    console.log('less')
+    subscribeForm.style.opacity = "0";
+    subscribeForm.style.maxHeight = "0";
+    subscribeForm.style.marginTop = "0";
+  } else {
+    subscribeForm.style.opacity = "1";
+    subscribeForm.style.maxHeight = "500px";
+  }
+
+  lastScrollTop = currentScroll
+}
+
+window.addEventListener('scroll', checkSubscribeVisibility);
